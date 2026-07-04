a79ac85a2db6b467a025ab9aefd74d2f02197b05
Jaroslaw-Baumgart <jaroslawbaumgart@gmail.com>
2026-07-04 03:45:59 +0200

    codecrafters submit [skip ci]

--- app/core/jobs/jobsCommand.ts
+++ app/core/jobs/jobsCommand.ts
@@ -3,14 +3,31 @@ import type { Job, JobStore } from "./jobStore";
 
 export function createJobsBuiltin(jobs: JobStore): BuiltinHandler {
   return ({ output }) => {
-    for (const job of jobs.list()) {
-      output.stdout.write(formatJob(job));
+      const entries = jobs.list();
+
+
+    for (const job of entries) {
+      output.stdout.write(formatJob(job, markerFor(job, entries)));
     }
 
     return { exitCode: 0 };
   };
 }
 
-function formatJob(job: Job): string {
-  return `[${job.id}]+  ${job.status.padEnd(24)}${job.command} &\n`;
+function markerFor(job: Job, jobs: readonly Job[]): string {
+  const index = jobs.indexOf(job);
+
+  if (index === jobs.length - 1) {
+    return "+";
+  }
+  
+  if (index === jobs.length - 2) {
+    return "-";
+  }
+
+  return " ";
+}
+
+function formatJob(job: Job, marker: string): string {
+  return `[${job.id}]${marker}  ${job.status.padEnd(24)}${job.command} &\n`;
 }
